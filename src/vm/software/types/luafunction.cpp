53a594bae0642677963f8091893152feec459494
Meepen <meepdarknessmeep@gmail.com>
2021-06-26 15:07:42 -0400

    convert big call args in luafunction to a runner state

--- src/vm/software/types/luafunction.cpp
+++ src/vm/software/types/luafunction.cpp
@@ -8,34 +8,44 @@ using namespace lorelai::vm;
 
 std::shared_ptr<object> lorelai::vm::function_metatable = nullptr;
 
-using func = state::_retdata *(*)(softwarestate &state, size_t &nextinstruction, int nrets, int nargs, const bytecode::instruction &instr, const std::shared_ptr<bytecode::prototype> proto, state::_retdata &retdata);
+struct _running {
+	softwarestate &state;
+	int nrets;
+	int nargs;
+	std::shared_ptr<bytecode::prototype> proto;
+	state::_retdata retdata {0, 0};
+	int multres = 0;
+	size_t nextinstruction = 0;
+};
 
-#define OPCODE_FUNCTION(t) static state::_retdata *t(softwarestate &state, size_t &nextinstruction, int nrets, int nargs, const bytecode::instruction &instr, const std::shared_ptr<bytecode::prototype> proto, state::_retdata &retdata)
+using func = state::_retdata *(*)(_running &run, const bytecode::instruction &instr);
+
+#define OPCODE_FUNCTION(t) static state::_retdata *t(_running &run, const bytecode::instruction &instr)
 
 OPCODE_FUNCTION(openvironmentget) {
-	objectcontainer index = std::make_shared<stringobject>(proto->strings(instr.b()));
+	objectcontainer index = std::make_shared<stringobject>(run.proto->strings(instr.b()));
 
-	state.registry->rawget(state, state[instr.a()], index);
+	run.state.registry->rawget(run.state, run.state[instr.a()], index);
 
 	return nullptr;
 }
 
 OPCODE_FUNCTION(opnumber) {
-	state[instr.a()] = std::make_shared<numberobject>(proto->numbers(instr.b()));
+	run.state[instr.a()] = std::make_shared<numberobject>(run.proto->numbers(instr.b()));
 
 	return nullptr;
 }
 
 OPCODE_FUNCTION(opcall) {
 	 // A .. A+C-2 = A(A+1 .. A + B)
-	auto old = state->pushpointer(state->base + instr.a());
-	auto data = state[0];
-	auto nret = data->call(state, instr.c() - 1, instr.b());
+	auto old = run.state->pushpointer(run.state->base + instr.a());
+	auto data = run.state[0];
+	auto nret = data->call(run.state, instr.c() - 1, instr.b());
 	if (instr.c() >= 1) {
-		state->poppointer(old, nret, state->base + instr.a(), instr.c() - 1);
+		run.state->poppointer(old, nret, run.state->base + instr.a(), instr.c() - 1);
 	}
 	else {
-		state->poppointer(old, nret, state->base + state->top, -1);
+		run.state->poppointer(old, nret, run.state->base + run.state->top, -1);
 	}
 
 	return nullptr;
@@ -47,7 +57,7 @@ OPCODE_FUNCTION(opmov) {
 	int a = instr.a(), b = instr.b();
 
 	for (int i = 0; i < instr.c(); i++) {
-		state[a + i] = state[b + i];
+		run.state[a + i] = run.state[b + i];
 	}
 	
 }
@@ -72,8 +82,7 @@ public:
 };
 
 state::_retdata luafunctionobject::call(softwarestate &state, int nrets, int nargs) {
-	auto oldtop = state->top;
-	state::_retdata retdata { 0, 0 };
+	_running run { state, nrets, nargs, data };
 
 	size_t nextinstruction = 0;
 	auto max = data->instructions_size();
@@ -84,10 +93,10 @@ state::_retdata luafunctionobject::call(softwarestate &state, int nrets, int nar
 		if (found == opcode_map.end()) {
 			throw exception("Unknown opcode " + bytecode::instruction_opcode_Name(instr.op()));
 		}
-		if (found->second(state, nextinstruction, nrets, nargs, instr, data, retdata)) {
+		if (found->second(run, instr)) {
 			break;
 		}
 	}
 
-	return retdata;
+	return run.retdata;
 }
