e4f089cc6210cfeec5b07c87f7d108955745ea37
Meepen <meepdarknessmeep@gmail.com>
2021-07-20 23:09:23 -0400

    revert test.lua and add CONST and NOTCONST test

--- tests/test.lua
+++ tests/test.lua
@@ -1,5 +1,19 @@
-local a, b = 1, 1
-do
-    a = 2
+local t = {1, [false] = true, [true] = false}
+
+t.a = "lmao"
+
+print(t[1], t[true], t[false], t.a)
+print(t[1], t[true], t[false], t.a)
+
+local function a()
+	print(1)
 end
-print(a)
+
+local CONST, NOTCONST = 1, 2
+do
+    NOTCONST = 4
+end
+
+print(CONST, NOTCONST)
+
+a()
